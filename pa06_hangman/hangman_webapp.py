00a4db4c928e26d773975334948d64c0899100e9
CynthiaMenna <62360931+CynthiaMenna@users.noreply.github.com>
2020-04-13 13:44:09 -0400

    Fixed indentations
    final product!

--- pa06_hangman/hangman_webapp.py
+++ pa06_hangman/hangman_webapp.py
@@ -39,39 +39,38 @@ def hangman():
         return play()
 
     elif request.method == 'POST':
-    letter = request.form['guess']
-    guesses = []
-    guesses.append(letter)
-    guesses= "".join(guesses)
-    word=state['word']
-    letter_length = False
-    already_guessed=False
-    won = False
-    if len(letter) > 1:
-        letter_length = True
-        print("Please only enter one letter.")
-    if letter in state['guesses']: # check if letter has already been guessed
-        already_guessed=True
-        print("you already guessed that.")
-        print("please guess again.") # and generate a response to guess again
-    elif letter in word: # else check if letter is in word
-        print("Yay! The letter is in the word.")
-    state['guesses'] += [letter]
-    word_so_far = hangman_methods.print_word(state)
-    state['word_so_far'] = word_so_far
-    if state['word_so_far']==state['word']: # then see if the word is complete
-        won = True
-        print('you won!')
-    elif letter not in word: # if letter not in word, then tell them
-        print("that letter is not in the word. try again.")
-    return render_template('play.html',
-                            state=state,
-                            letter=letter,
-                            letter_length=letter_length,
-                            word=state['word'],
-                            guesses=state['guesses'],
-                            already_guessed=already_guessed,
-                            won=won)
+        letter = request.form['guess']
+        guesses = []
+        guesses.append(letter)
+        guesses= "".join(guesses)
+        word=state['word']
+        letter_length = False
+        already_guessed=False
+        won = False
+        if len(letter) > 1:
+            letter_length = True
+            print("Please only enter one letter.")
+        if letter in state['guesses']: # check if letter has already been guessed
+            already_guessed=True
+            print("you already guessed that.")
+            print("please guess again.") # and generate a response to guess again
+        elif letter in word: # else check if letter is in word
+            print("Yay! The letter is in the word.")
+        state['guesses'] += [letter]
+        word_so_far = hangman_methods.print_word(state)
+        state['word_so_far'] = word_so_far
+        if state['word_so_far']==state['word']: # then see if the word is complete
+            won = True
+            print('you won!')
+        elif letter not in word: # if letter not in word, then tell them
+            print("that letter is not in the word. try again.")
+        return render_template('play.html',state=state,
+                                            letter=letter,
+                                            letter_length=letter_length,
+                                            word=state['word'],
+                                            guesses=state['guesses'],
+                                            already_guessed=already_guessed,
+                                            won=won)
 
 @app.route('/about')
 def about():
